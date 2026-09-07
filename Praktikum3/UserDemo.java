c5dc61a4c17b74408fbfcbbc82821750cc2eb17f
artasdaa <cellom293@gmail.com>
2026-09-07 08:39:06 +0700

    Percobaan1

---
+++ Praktikum3/UserDemo.java
@@ -0,0 +1,9 @@
+package Praktikum3;
+
+public class UserDemo {
+    public static void main(String[] args) {
+        User user1 = new User("marcello.artasda", "marcello.artasda@gmail.com");
+        user1.displayInfo();
+    }
+    
+}
