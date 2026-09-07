4ba37989b57b7bb707cc5f12c3f7a053784e7281
artasdaa <cellom293@gmail.com>
2026-09-07 09:41:01 +0700

    Tugas

---
+++ Praktikum3/Anggota.java
@@ -0,0 +1,50 @@
+package Praktikum3;
+
+public class Anggota {
+
+    private String nomorKTP;
+    private String nama;
+    private int limitPinjaman;
+    private int jumlahPinjaman;
+
+    public Anggota(String nomorKTP, String nama, int limitPinjaman) {
+        this.nomorKTP = nomorKTP;
+        this.nama = nama;
+        this.limitPinjaman = limitPinjaman;
+        this.jumlahPinjaman = 0;
+    }
+
+    public String getNomorKTP() {
+        return nomorKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getLimitPinjaman() {
+        return limitPinjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(int jumlah) {
+        if (jumlahPinjaman + jumlah > limitPinjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            jumlahPinjaman += jumlah;
+        }
+    }
+
+    public void angsur(int jumlah) {
+        if (jumlahPinjaman == 0) {
+            System.out.println("Tidak ada pinjaman yang harus diangsur.");
+        } else if (jumlah > jumlahPinjaman) {
+            System.out.println("Maaf, jumlah angsuran melebihi jumlah pinjaman.");
+        } else {
+            jumlahPinjaman -= jumlah;
+        }
+    }
+}
